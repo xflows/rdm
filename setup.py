9e1dbd0375261a4201c33c98523005ad1ae6c0ea
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-01-18 12:46:47 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='python-rdm',
-    version='0.1.4',
+    version='0.1.4a',
     packages=['rdm'],
     include_package_data=True,
     license='MIT License',
