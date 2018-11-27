d386059e54385f6640207c96879820858c2685fa
skblaz <skrljblaz@gmail.com>
2018-11-27 11:07:26 +0100

    added more datasets

--- benchmark_studies/generate_jobs.py
+++ benchmark_studies/generate_jobs.py
@@ -1,11 +1,17 @@
 ## A simple python job generator
 
 learners = ['aleph','RSD','treeliker','wordification']
-dbtuples = [('trains','trains','direction','east'), ## trains data set
-            ('Carcinogenesis','canc','class','TODO'), ## cancer data set
+dbtuples = [('trains','trains','direction'), ## trains data set
+            ('Carcinogenesis','canc','class'), ## cancer data set
+            ('Biodegradability','molecule','logp'),
+            ('genes','Classification','Localization'),
+            ('Facebook','feat','gender1'),
+            ('NCAA','target','team_id1_wins'),            
+            ('Pyrimidine','molecule','activity'),
+            ('CORA','paper','class_label')
 ]
-result_file = "./tmp_results.txt"
 
+result_file = "./tmp_results.txt"
 for dtuple in dbtuples:
     for learner in learners:
-        print ('python','benchmark_algorithms.py','--dataset',dtuple[0],'--target_label',dtuple[1],'--target_attribute',dtuple[2],'>',result_file)
+        print ('python','benchmark_algorithms.py','--dataset',dtuple[0],'--target_table',dtuple[1],'--target_label',dtuple[2],'>',result_file)
