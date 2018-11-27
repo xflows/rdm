8d1b623a45bb8688f082ffd3b44363a4e9347bae
skblaz <skrljblaz@gmail.com>
2018-11-27 10:48:25 +0100

    added conda lib installer

---
+++ configure.sh
@@ -0,0 +1,6 @@
+sudo apt-get install yum
+sudo apt-get install mysql-client mysql-server
+pip3 install arff
+conda install -y  orange
+conda install -y mysql
+conda install -y mysql-connector-python
