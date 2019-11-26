39da12a816a98733c61f5522fbeac0f829b71e09
vid podpecan <vid.podpecan@ijs.si>
2019-11-26 16:07:32 +0100

    Fixed as in the Docs.

--- examples/imdb.py
+++ examples/imdb.py
@@ -1,9 +1,4 @@
-import sys
-
-reload(sys)  # Reload does the trick!
-sys.setdefaultencoding('UTF8')
-
-import orange
+import Orange
 
 from rdm.db import DBVendor, DBConnection, DBContext, RSDConverter, mapper
 from rdm.wrappers import RSD
@@ -25,6 +20,9 @@ context = DBContext(connection, target_table='movies', target_att='quality')
 # Cross-validation loop
 predictions = []
 folds = 10
+
+a = cv_split(context, folds=folds, random_seed=0)
+
 for train_context, test_context in cv_split(context, folds=folds, random_seed=0):
     # Find features on the train set
     conv = RSDConverter(train_context)
@@ -35,21 +33,25 @@ for train_context, test_context in cv_split(context, folds=folds, random_seed=0)
         cn2sd=False                    # Disable built-in subgroup discovery
     )
 
+    a = conv.background_knowledge()
+    b = conv.all_examples()
+
     # Train the classifier on the *train set*
     train_data = arff_to_orange_table(train_arff)
-    tree_classifier = orange.TreeLearner(train_data, max_depth=5)
+    tree_learner = Orange.classification.TreeLearner(max_depth=5)
+    tree_classifier = tree_learner(train_data)
 
     # Map the *test set* using the features from the train set
     test_arff = mapper.domain_map(features, 'rsd', train_context, test_context, format='arff')
 
     # Classify the test set
     test_data = arff_to_orange_table(test_arff)
-    fold_predictions = [(ex[-1], tree_classifier(ex)) for ex in test_data]
+    fold_predictions = [(ex.get_class(), tree_classifier(ex)) for ex in test_data]
     predictions.append(fold_predictions)
 
 acc = 0
 for fold_predictions in predictions:
-    acc += sum([1.0 for actual, predicted in fold_predictions if actual == predicted])/len(fold_predictions)
-acc = 100 * acc/folds
+    acc += sum([1.0 for actual, predicted in fold_predictions if actual == predicted]) / len(fold_predictions)
+acc = 100 * acc / folds
 
 print('Estimated predictive accuracy: {0:.2f}%'.format(acc))
