b0d2d0ec0a6b0e4a6d4e6634d7c63832aa7fa4d8
vid podpecan <vid.podpecan@ijs.si>
2019-11-26 14:52:16 +0100

    Completely new ARFF to Orange Table conversion. Uses liac-arff package.

--- rdm/helpers.py
+++ rdm/helpers.py
@@ -1,18 +1,71 @@
-import tempfile
-import orange
+import arff
+import Orange
 
 
-def arff_to_orange_table(arff):
+def arffheader2domain(header, target_idx=[-1]):
     '''
-    Convert a string in arff format to an Orange table.
+    Constructs Orange.data.Domain from ARFF header.
+    See https://www.cs.waikato.ac.nz/~ml/weka/arff.html
 
-        :param arff: string in arff format
+    Parameters
+    ----------
+    header : dict
+        ARFF header as returned by the ``arff.load`` function
+    classes_idx : list, optional
+        List of indices of target attributes. The default is [-1] (last attribute is the class)
 
-        :return: Orange data table object constructed from the arff string
-        :rtype: orange.ExampleTable
+    Returns
+    -------
+    Orange.data.Domain
+
     '''
-    with tempfile.NamedTemporaryFile(suffix='.arff', delete=True) as f:
-        f.write(arff)
-        f.flush()
-        table = orange.ExampleTable(f.name)
-        return table
+    varlist = []
+    classes = []
+    metas = []
+    for aname, atype in header:
+        if isinstance(atype, list):
+            var = Orange.data.DiscreteVariable(name=aname, values=atype)
+        else:
+            atype = atype.lower()
+            if atype in ['numeric', 'real', 'float', 'double']:
+                var = Orange.data.ContinuousVariable(name=aname)
+            elif atype in ['date']:
+                var = Orange.data.TimeVariable(name=aname)
+            elif atype in ['string', 'text']:
+                var = Orange.data.StringVariable(name=aname)
+            elif atype in ['integer', 'int']:
+                var = Orange.data.ContinuousVariable(name=aname)
+            else:
+                var = Orange.data.StringVariable(name=aname)
+        if isinstance(var, Orange.data.StringVariable):
+            metas.append(var)
+        varlist.append(var)
+
+    for cid in target_idx:
+        classes.append(varlist[cid])
+    for cl in classes:
+        varlist.remove(cl)
+    for meta in metas:
+        varlist.remove(meta)
+    return Orange.data.Domain(varlist, class_vars=classes, metas=metas)
+
+
+def arff_to_orange_table(arff_data):
+    '''
+    Constructs Orange.data.Table from ARFF data stored in a string
+
+    Parameters
+    ----------
+    arff_data : str
+        ARFF file stored in a string.
+    Returns
+    -------
+    table : Orange.data.Table
+        Orange data table with the given domain and data. String attributes are stored as meta attributes.
+
+    '''
+    arff_description = arff.loads(arff_data)
+    domain = arffheader2domain(arff_description['attributes'])
+    table = Orange.data.Table.from_list(domain, arff_description['data'])
+    table.name = arff_description['relation']
+    return table
